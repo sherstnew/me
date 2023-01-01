531e8b4ea133240e8ad2caabcbec2f7acaa7bf00
sherstnew <sherstnev.denis.v@gmail.com>
2023-01-01 11:52:29 +0300

    add denis info

---
+++ src/static/aboutMe/denis.js
@@ -0,0 +1,15 @@
+export const denis = {
+  name: 'Денис',
+  age: 14,
+  home: 'Москва, Россия',
+  projects: [
+
+  ],
+  techs: [
+    {
+      techName: '',
+      techHours: '',
+      techLogo: '',
+    },
+  ],
+};
